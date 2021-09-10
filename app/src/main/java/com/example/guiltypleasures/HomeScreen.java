51c18e0d111aeeac50f38f41c3a544267a89e279
Jacob Heiser <dialwolf.windrider@gmail.com>
2021-09-09 20:37:47 -0400

    Implemented API with Layout updates

--- app/src/main/java/com/example/guiltypleasures/HomeScreen.java
+++ app/src/main/java/com/example/guiltypleasures/HomeScreen.java
@@ -3,18 +3,15 @@ package com.example.guiltypleasures;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Notification;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,121 +27,126 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
-
-    private ActionBar toolbar;
+    //JSON Link
     private static String JSON_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=8ef07998664a58a01082bc2ab507fcb8";
 
+    //variables
     List<UpcomingMovieClass> upcomingMovies;
+
+    //user interface variables
     RecyclerView recyclerView;
 
+
+    //create Home Screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+
+        //set and create variables
         upcomingMovies = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
-        toolbar = getSupportActionBar();
 
-        //GetData getData = new GetData();
-        //getData.execute();
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        bottomNav.setOnNavigationItemSelectedListener(NavigationListener);
-
+        //create object of class and execute methods
+        GetData getData = new GetData();
+        getData.execute();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener NavigationListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                    switch (item.getItemId())
-                    {
-                        case R.id.homepage:
-                            startActivity(new Intent(HomeScreen.this, HomeScreen.class));
-                            HomeScreen.this.finish();
-                            break;
-                        case R.id.database:
-                            startActivity(new Intent(HomeScreen.this, Database.class));
-                            HomeScreen.this.finish();
-                            break;
-                        case R.id.list:
-                            startActivity(new Intent(HomeScreen.this, List.class));
-                            HomeScreen.this.finish();
-                            break;
-                        case R.id.profile:
-                            startActivity(new Intent(HomeScreen.this, UserProfile.class));
-                            HomeScreen.this.finish();
-                            break;
-                    }
-
-                    return true;
-                }
-            };
+    //GetData Class
     public class GetData extends AsyncTask<String, String , String> {
-
         @Override
         protected String doInBackground(String... strings) {
+            //variable
             String current = "";
 
             try {
+                //URL & HTTP variables
                 URL url;
                 HttpURLConnection urlConnection = null;
+
                 try {
+                    //create URL with JSON connection and initialize connection
                     url = new URL(JSON_URL);
                     urlConnection = (HttpURLConnection) url.openConnection();
+
+                    //Gather Input Data from URL connection
                     InputStream is = urlConnection.getInputStream();
                     InputStreamReader read = new InputStreamReader(is);
 
+                    //Read Data Gathered until end
                     int data = read.read();
-
                     while (data != -1) {
                         current += (char) data;
                         data = read.read();
                     }
+
+                    //return Results
                     return current;
-                } catch (MalformedURLException e) {
+                }
+                //catch errors of 2nd Try Statement
+                catch (MalformedURLException e) {
                     e.printStackTrace();
-                } catch (IOException e) {
+                }
+                catch (IOException e) {
                     e.printStackTrace();
-                } finally {
+                }
+                //Do this regardless of results of 2nd Try Statement
+                finally {
+                    //make sure connection is disabled
                     if (urlConnection != null) {
                         urlConnection.disconnect();
                     }
                 }
-            } catch (Exception e) {
+            }
+            //catch errors of 1st Try Statement
+            catch (Exception e) {
                 e.printStackTrace();
             }
+
+            //return Results
             return current;
         }
 
         @Override
         protected void onPostExecute(String s) {
             try {
+                //create a JSON object of String Parameter
                 JSONObject jsonObject = new JSONObject(s);
+
+                //create Json Array based on name on list's name
                 JSONArray jsonArray = jsonObject.getJSONArray("results");
 
+                //for loop to add all objects to Movie Array List
                 for(int i = 0; i < jsonArray.length(); i++) {
+                    //Create movie Json Object
                     JSONObject jsonObject1 = jsonArray.getJSONObject(i);
                     UpcomingMovieClass up = new UpcomingMovieClass();
 
-                    up.setId(jsonObject1.getString("vote_average"));
-                    up.setTitle(jsonObject1.getString("title"));
-                    up.setImg(jsonObject1.getString("post_path"));
+                    //set Properties
+                    up.setId(jsonObject1.getString("title"));
+                    up.setTitle(jsonObject1.getString("vote_average"));
+                    up.setImg(jsonObject1.getString("poster_path"));
 
+                    //add to Array List
                     upcomingMovies.add(up);
                 }
 
-            } catch (JSONException e) {
+            }
+            //catch errors of Try Statement
+            catch (JSONException e) {
                 e.printStackTrace();
             }
-            PutDataIntoRecyclerView(upcomingMovies);
 
+            //Push data into View
+            PutDataIntoRecyclerView(upcomingMovies);
         }
     }
+
     private void PutDataIntoRecyclerView(List<UpcomingMovieClass> upcomingMovies) {
-        MovieAdapter adapter = new MovieAdapter(this);
+        //create object of adapter and set it in the recycler view
+        MovieAdapter adapter = new MovieAdapter(this, upcomingMovies);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
--- app/src/main/java/com/example/guiltypleasures/MovieAdapter.java
+++ app/src/main/java/com/example/guiltypleasures/MovieAdapter.java
@@ -14,46 +14,55 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+//Main Class extending RecyclerView for MovieAdapter
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    //variables
     private Context mContext;
     private List<UpcomingMovieClass> mData;
 
-    public MovieAdapter(Context mContext) {
+    //constructor
+    public MovieAdapter(Context mContext, List<UpcomingMovieClass> mData) {
         this.mContext = mContext;
         this.mData = mData;
     }
 
+
     @NonNull
     @Override
-    public MovieAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //call view and import it
         View v;
         LayoutInflater inflater = LayoutInflater.from(mContext);
         v = inflater.inflate(R.layout.upcoming_movies, parent, false);
 
-        return new MovieAdapter.MyViewHolder(v);
+        return new MyViewHolder(v);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull MovieAdapter.MyViewHolder holder, int position) {
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        //create holder and initiate created movie variable widgets
         holder.id.setText(mData.get(position).getId());
         holder.title.setText(mData.get(position).getTitle());
         Glide.with(mContext).load("https://image.tmdb.org/t/p/w500" + mData.get(position).getImg()).into(holder.img);
-
-
     }
+
 
     @Override
     public int getItemCount() {
+        //return size of data
         return mData.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
 
+    //Inner Class extending RecyclerView
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        //pass movie variable widgets
         TextView id;
         TextView title;
         ImageView img;
 
+        //constructor
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
